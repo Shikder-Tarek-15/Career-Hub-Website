4af6f9ddd3ebc212e8aa1249557b26fcf95a62ae
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-03-23 01:34:55 +0600

    Add nav and footer

---
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,9 @@
+const AppliedJobs = () => {
+  return (
+    <div>
+      <h2>This is Applied jobs</h2>
+    </div>
+  );
+};
+
+export default AppliedJobs;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppliedJobs from "./Components/AppliedJobs/AppliedJobs";
+import Blog from "./Components/Blog/Blog";
 import Home from "./Components/Home/Home";
+import Root from "./Components/Root/Root";
+import Statistics from "./Components/Statistics/Statistics";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
+    element: <Root></Root>,
     children: [
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
       },
     ],
   },
